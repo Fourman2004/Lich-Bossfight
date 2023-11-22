183ac0539b056301f1e2c47ec604796069d6bfcd
MARSHALL SHARP <MS279226@falmouth.ac.uk>
2023-11-22 14:51:45 +0000

    Firebolt hurts player

---
+++ Game/Source/GAM206/Private/Health_Orb.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Health_Orb.h"
+
+// Sets default values
+AHealth_Orb::AHealth_Orb()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHealth_Orb::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHealth_Orb::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
