ab99ae56016d64af3faff3dd1a90d0ca86fc84bc
MARSHALL SHARP <MS279226@falmouth.ac.uk>
2023-12-12 15:23:54 +0000

    Converting Randattack to CPP

---
+++ Game/Source/GAM206/Public/CPP_RandAtk.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "CPP_RandAtk.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM206_API UCPP_RandAtk : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
