db6eca0ddf8c796308cdd5401adc7943a1768db0
MARSHALL SHARP <MS279226@falmouth.ac.uk>
2023-12-14 15:28:37 +0000

    end day

---
+++ Game/Source/GAM206/Public/Boss_BehaviourTree.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "Boss_BehaviourTree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM206_API UBoss_BehaviourTree : public UBehaviorTree
+{
+	GENERATED_BODY()
+	
+};
